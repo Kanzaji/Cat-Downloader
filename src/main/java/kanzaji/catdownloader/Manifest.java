a605bdec7e7858dff6b63268ccdd445ce8c6e02d
Kanzaji ^^ <60540476+Kanzaji@users.noreply.github.com>
2022-11-26 15:11:34 +0100

    Replace all spaces in the final URL to fix an issue with few mods using spaces in their file name

--- src/main/java/kanzaji/catdownloader/Manifest.java
+++ src/main/java/kanzaji/catdownloader/Manifest.java
@@ -34,6 +34,7 @@ public class Manifest {
                         if (file.id.intValue() == fileID.intValue()) {
                             String url_2 = "https://edge.forgecdn.net/files/" + String.valueOf(file.id).substring(0, 4) + "/" + String.valueOf(file.id).substring(4) + "/" + file.name;
                             // System.out.println(url_2);
+                            url_2.replaceAll(" ", "%20");
                             return url_2;
                         }
                     }
